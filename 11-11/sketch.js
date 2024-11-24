977bd9f5b9fe20fefd739bdb78afbc0fc98ff30b
ooferini <mintimalspace@gmail.com>
2024-11-24 14:54:49 -0500

    classwork

---
+++ 11-11/sketch.js
@@ -0,0 +1,41 @@
+let allTheText;
+let theFont;
+let currentLine = ''; //empty string
+let textPositions = []; //empty array one to hold the text positions
+let charSize = []; // empty array to hold the sizes of the text
+
+function preload(){
+  allTheText = loadStrings('data/aliceinwonderland.txt');  // this creates an array of strings, with each individual line being it's one entry into the array
+  theFont = loadFont('data/font2.otf');
+}
+
+function setup() {
+  createCanvas(1200, 1200);
+  textFont(theFont);
+  getLine();
+}
+
+function draw() {
+  background(255);
+  let positionX = 0;
+
+  for(let i = 0; i < currentLine.length; i++){
+   
+  }
+}
+
+
+function getLine(){
+    
+
+    
+
+    for(let i = 0; i < currentLine.length; i++){
+      textPositions[i] = 0;
+      charSizes[i] = random(45, 70);
+    }
+}
+
+function keyReleased(){
+ 
+}
